7639a69ff1db6a2c2e78c97497f7b449cac0db06
Dias <dias.atudinov@gmail.com>
2025-12-18 02:11:03 +0500

    add menu, onboarding and loader views

--- Amazing Woman Wardrobe/Amazing Woman Wardrobe/AWModelues/AWMenu/AWMenuView.swift
+++ Amazing Woman Wardrobe/Amazing Woman Wardrobe/AWModelues/AWMenu/AWMenuView.swift
@@ -2,17 +2,106 @@
 //  AWMenuView.swift
 //  Amazing Woman Wardrobe
 //
-//  Created by Dias Atudinov on 18.12.2025.
 //
 
 import SwiftUI
 
-struct AWMenuView: View {
+struct AWMenuContainer: View {
+    
+    @AppStorage("firstOpenBB") var firstOpen: Bool = true
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ZStack {
+                if firstOpen {
+                    AWOnboardingView(getStartBtnTapped: {
+                        firstOpen = false
+                    })
+                } else {
+                    AWMenuView()
+                }
+            }
+        }
     }
 }
 
+struct AWMenuView: View {
+    @State var selectedTab = 0
+//    @StateObject var diveViewModel = BBMyDivesViewModel()
+    private let tabs = ["My dives", "Calendar", "Stats"]
+    
+    var body: some View {
+        ZStack {
+            
+            switch selectedTab {
+            case 0:
+                Color.black.ignoresSafeArea()
+            case 1:
+                Color.secondaryText.ignoresSafeArea()
+            case 2:
+                Color.bg.ignoresSafeArea()
+            default:
+                Text("default")
+            }
+            VStack {
+                Spacer()
+                
+                HStack {
+                    ForEach(0..<tabs.count) { index in
+                        Button(action: {
+                            selectedTab = index
+                        }) {
+                            VStack(spacing: 4) {
+                                Image(selectedTab == index ? selectedIcon(for: index) : icon(for: index))
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(height: 44)
+                                
+                                Text(text(for: index))
+                                    .font(.system(size: 16, weight: .semibold))
+                                    .foregroundStyle(selectedTab == index ? .buttons : .calendar)
+                            }
+                            .frame(maxWidth: .infinity)
+                            
+                        }
+                    }
+                }.padding(.horizontal, 32)
+            }
+            .padding(.bottom, 30)
+                .ignoresSafeArea()
+            
+            
+        }
+    }
+    
+    private func icon(for index: Int) -> String {
+        switch index {
+        case 0: return "tab1IconAW"
+        case 1: return "tab2IconAW"
+        case 2: return "tab3IconAW"
+        default: return ""
+        }
+    }
+    
+    private func selectedIcon(for index: Int) -> String {
+        switch index {
+        case 0: return "tab1IconSelectedAW"
+        case 1: return "tab2IconSelectedAW"
+        case 2: return "tab3IconSelectedAW"
+        default: return ""
+        }
+    }
+    
+    private func text(for index: Int) -> String {
+        switch index {
+        case 0: return "Outfits"
+        case 1: return "Wardrobe"
+        case 2: return "Calendar"
+        default: return ""
+        }
+    }
+}
+
+
 #Preview {
-    AWMenuView()
+    AWMenuContainer()
 }
--- Amazing Woman Wardrobe/Amazing Woman Wardrobe/AWModelues/AWMenu/AWOnboardingView.swift
+++ Amazing Woman Wardrobe/Amazing Woman Wardrobe/AWModelues/AWMenu/AWOnboardingView.swift
@@ -2,17 +2,159 @@
 //  AWOnboardingView.swift
 //  Amazing Woman Wardrobe
 //
-//  Created by Dias Atudinov on 18.12.2025.
 //
 
 import SwiftUI
 
 struct AWOnboardingView: View {
+    var getStartBtnTapped: () -> ()
+    @State var count = 0
+    
+    var onbImage: Image {
+        switch count {
+        case 0:
+            Image(.onboardingImg1AW)
+        case 1:
+            Image(.onboardingImg2AW)
+        case 2:
+            Image(.onboardingImg3AW)
+        default:
+            Image(.onboardingImg1AW)
+        }
+    }
+    
+    var onbTitle: String {
+        switch count {
+        case 0:
+            "Your wardrobe in your\npocket"
+        case 1:
+            "Never stand in front of\nyour closet again"
+        case 2:
+            "Amazing Woman:\nWardrobe"
+        default:
+            ""
+        }
+    }
+    
+    var onbDescription: String {
+        switch count {
+        case 0:
+            "Add items, create stylish outfits, and plan what to wear — no morning stress."
+        case 1:
+            "All your clothes and ready-made looks are always with you. Choose an outfit in seconds."
+        case 2:
+            "Your personal stylist in your phone: clothing catalog, outfits, and an outfit calendar."
+        default:
+            ""
+        }
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 32) {
+            onbImage
+                .resizable()
+                .scaledToFit()
+                .ignoresSafeArea(edges: .top)
+                .overlay(alignment: .bottom) {
+                    VStack(alignment: .leading, spacing: 12) {
+                        Text(onbTitle)
+                            .font(.system(size: 32, weight: .bold))
+                            .fixedSize()
+                        Text(onbDescription)
+                            .font(.system(size: 15, weight: .bold))
+                            
+                        
+                    }
+                    .frame(maxWidth: .infinity)
+                    .multilineTextAlignment(.leading)
+                    .foregroundStyle(.white)
+                    .padding(.horizontal)
+                    .padding(.bottom, 16)
+                }
+            
+            
+            VStack(spacing: 12) {
+                
+                HStack {
+                    if count == 0 {
+                        Rectangle()
+                            .fill(.calendar)
+                            .frame(width: 25, height: 12)
+                            .clipShape(RoundedRectangle(cornerRadius: 20))
+                            .overlay {
+                                RoundedRectangle(cornerRadius: 20)
+                                    .stroke(lineWidth: 1)
+                                    .foregroundStyle(.white)
+                            }
+                    } else {
+                        Circle()
+                            .stroke(lineWidth: 2)
+                            .fill(.white)
+                            .frame(width: 12, height: 12)
+                            .clipShape(RoundedRectangle(cornerRadius: 20))
+                    }
+                    
+                    
+                    if count == 1 {
+                        Rectangle()
+                            .fill(.calendar)
+                            .frame(width: 25, height: 12)
+                            .clipShape(RoundedRectangle(cornerRadius: 20))
+                            .overlay {
+                                RoundedRectangle(cornerRadius: 20)
+                                    .stroke(lineWidth: 1)
+                                    .foregroundStyle(.white)
+                            }
+                    } else {
+                        Circle()
+                            .stroke(lineWidth: 2)
+                            .fill(.white)
+                            .frame(width: 12, height: 12)
+                            .clipShape(RoundedRectangle(cornerRadius: 20))
+                    }
+                    
+                    if count == 2 {
+                        Rectangle()
+                            .fill(.calendar)
+                            .frame(width: 25, height: 12)
+                            .clipShape(RoundedRectangle(cornerRadius: 20))
+                            .overlay {
+                                RoundedRectangle(cornerRadius: 20)
+                                    .stroke(lineWidth: 1)
+                                    .foregroundStyle(.white)
+                            }
+                    } else {
+                        Circle()
+                            .stroke(lineWidth: 2)
+                            .fill(.white)
+                            .frame(width: 12, height: 12)
+                            .clipShape(RoundedRectangle(cornerRadius: 20))
+                    }                }
+                
+                Button {
+                    if count < 2 {
+                        withAnimation {
+                            count += 1
+                        }
+                    } else {
+                        getStartBtnTapped()
+                    }
+                } label: {
+                    Text(count < 2 ? "Next" : "Get Started")
+                        .font(.system(size: 36, weight: .semibold))
+                        .foregroundStyle(.white)
+                        .padding(.vertical, 13)
+                        .frame(width: 220)
+                        .background(.buttons)
+                        .clipShape(RoundedRectangle(cornerRadius: 16))
+                }
+            }
+        }.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+            .background(.bg)
+            .ignoresSafeArea()
     }
 }
 
 #Preview {
-    AWOnboardingView()
+    AWOnboardingView(getStartBtnTapped: {})
 }
