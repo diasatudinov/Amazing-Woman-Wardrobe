89156e5026c570c890f092a4798f1027f8d12370
Dias <dias.atudinov@gmail.com>
2025-12-23 03:25:49 +0500

    new files

---
+++ Amazing Woman Wardrobe/Amazing Woman Wardrobe/AWModelues/AWCalendar/AWCalendarView.swift
@@ -0,0 +1,18 @@
+//
+//  AWCalendarView.swift
+//  Amazing Woman Wardrobe
+//
+//  Created by Dias Atudinov on 23.12.2025.
+//
+
+import SwiftUI
+
+struct AWCalendarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AWCalendarView()
+}
---
+++ Amazing Woman Wardrobe/Amazing Woman Wardrobe/AWModelues/AWCalendar/AWNewEventView.swift
@@ -0,0 +1,18 @@
+//
+//  AWNewEventView.swift
+//  Amazing Woman Wardrobe
+//
+//  Created by Dias Atudinov on 23.12.2025.
+//
+
+import SwiftUI
+
+struct AWNewEventView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AWNewEventView()
+}
